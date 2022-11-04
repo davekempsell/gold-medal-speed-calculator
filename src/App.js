0da4deec2bdd3fc5e36e8acec25db5eb51ec36fe
davekempsell <dave_kempsell@hotmail.com>
2022-11-04 19:04:11 +0000

    project set up, header created, responsive drop down menu for boat-type created and refactored into own component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { boatTypeMenu } from './components/boatType'
 
 function App() {
+  const [boatType, setBoatType] = useState('1x')
+
+  const selectBoatType = boat => {
+    setBoatType(boat.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>%Gold Medal Time Calculator</h1>
       </header>
+      <main>
+        <div class="boat-type">
+        {boatTypeMenu(selectBoatType)}
+        <p>{boatType}</p>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/boatType.js
@@ -0,0 +1,29 @@
+function boatTypeButton(boat, callback) {
+  return (
+    <button 
+      type="submit" 
+      key={`boatTypeButton-${boat}`}
+      value={boat} 
+      onClick={callback}>
+        {boat}
+    </button>
+  )
+}
+
+export function boatTypeMenu(callback) {
+
+  const boatTypes = ['1x', '2x', '4x', '2-', '4-', '4+', '8+']
+
+  return (
+    <div class="dropdown">
+      <button class="dropbtn">Boat Type</button>
+      <div class="dropdown-content">
+        {boatTypes.map(boat => {
+          return (
+            boatTypeButton(boat, callback)
+          )
+        })}
+      </div>
+    </div>
+  )
+}
